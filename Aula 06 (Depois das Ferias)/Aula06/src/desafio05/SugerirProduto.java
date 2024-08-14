4fe52efc9b0d9235a60ce382406b72f108a5f957
Luiz192512 <158178534+Luiz192512@users.noreply.github.com>
2024-08-14 11:37:56 -0300

    Aula 06

---
+++ Aula 06 (Depois das Ferias)/Aula06/src/desafio05/SugerirProduto.java
@@ -0,0 +1,31 @@
+package desafio05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SugerirProduto {
+	private int id;
+	private List<Produto> listaDeProdutos;
+	private String criterios;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Produto> getListaDeProdutos() {
+		return listaDeProdutos;
+	}
+	public void setListaDeProdutos(List<Produto> listaDeProdutos) {
+		this.listaDeProdutos = listaDeProdutos;
+	}
+	public String getCriterios() {
+		return criterios;
+	}
+	public void setCriterios(String criterios) {
+		this.criterios = criterios;
+	}
+	public List<Produto> sugerir() {
+	return new ArrayList<>();
+	}
+}
