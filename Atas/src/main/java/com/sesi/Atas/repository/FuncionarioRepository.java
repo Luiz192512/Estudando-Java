e94e380d50c94cf04e97603f4bc1386c7d73ae40
Luiz192512 <158178534+Luiz192512@users.noreply.github.com>
2024-10-15 11:59:08 -0300

    Delete Atas directory

--- Atas/src/main/java/com/sesi/Atas/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sesi.Atas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sesi.Atas.model.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
-
